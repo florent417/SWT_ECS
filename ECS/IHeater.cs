0d63dcba403fd23dbd2f88261650c2deced8657b
florent417 <flole@live.dk>
2019-02-12 15:01:03 +0100

    New application started, with new design. Constructor injection added and Interfaces added

---
+++ ECS/IHeater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECS
+{
+    public interface IHeater
+    {
+        bool RunSelfTest();
+
+        void TurnOff();
+
+        void TurnOn();
+    }
+}
---
+++ ECS/ITempSensor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECS
+{
+    public interface ITempSensor
+    {
+        int GetTemp();
+
+        bool RunSelfTest();
+    }
+}
