ea74bb85562177e77705623ca4d2dd3ced82d2f8
florent417 <flole@live.dk>
2019-02-12 13:55:26 +0100

    Added class diagram

---
+++ ECS.Legacy/ITempSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECS.Legacy
+{
+    public class ITempSensor
+    {
+        private int gen;
+
+        public void GetTemp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RunSelfTest()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
